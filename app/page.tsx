d06092edf25c3fc11fda74938bdcdb9db493a981
Nghiep <me@nghiep.dev>
2024-06-27 18:13:43 +0700

    Add sections

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         alt=''
         className='w-screen object-contain'
       />
+
       <div className='relative'>
         <Image
           src={img2}
@@ -36,8 +37,8 @@ export default function Home() {
               <img src='/telegram.svg' alt='' />
             </a>
           </div>
-          <div className='mt-auto mb-10 px-5 sm:px-10 xl:px-0'>
-            <h2 className='text-center text-white text-4xl sm:text-5xl xl:text-7xl'>
+          <div className='mt-auto mb-10 px-5'>
+            <h2 className='text-center text-white text-5xl sm:text-7xl'>
               -- OFFICIAL BURATINO CONTRACT --
             </h2>
             <div className='flex justify-center relative mt-10'>
@@ -49,6 +50,66 @@ export default function Home() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className='relative'>
+        <Image
+          src={img3}
+          quality={100}
+          priority
+          placeholder='blur'
+          alt=''
+          className='w-screen object-contain'
+        />
+        <div className='absolute inset-0'>
+          <div className='max-xl:pl-10 absolute right-5 bottom-5 sm:bottom-10 md:bottom-20 w-full max-w-[845px] text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center space-y-5 sm:space-y-10 max-xl:text-pretty'>
+            <p>
+              Get a share of Earth's richest man -now with a goated contract
+              address!
+            </p>
+            <p>
+              Starting with 69420, sacred number on Xand previous cycle top,
+              $ELON contractstands superior among its peers.
+            </p>
+            <p>Wanna stop missing out?</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='relative'>
+        <Image
+          src={img4}
+          quality={100}
+          priority
+          placeholder='blur'
+          alt=''
+          className='w-screen object-contain'
+        />
+        <div className='absolute inset-0'>
+          <div className='text-primary lg:mt-20 lg:ml-20 uppercase text-center inline-block max-lg:multi-[`text-center;w-full;pt-10`]'>
+            <h2 className='text-7xl sm:text-8xl md:text-9xl'>tokenomics</h2>
+            <p className='text-6xl sm:text-7xl md:text-8xl'>
+              supply: 200.000.000
+            </p>
+            <p className='text-6xl sm:text-7xl'>no tax - no bullshit</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='relative'>
+        <Image
+          src={img5}
+          quality={100}
+          priority
+          placeholder='blur'
+          alt=''
+          className='w-screen object-contain'
+        />
+        <div className='absolute inset-0 flex justify-center items-baseline'>
+          <a href='/' target='_buy' className='px-5 sm:px-0'>
+            <img src='/buy-now.svg' alt='' className='-translate-y-1/2' />
+          </a>
         </div>
       </div>
     </main>
